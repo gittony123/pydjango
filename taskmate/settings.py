1e5b5a832a3edae24677e8d4daaab438907cadcc
anthony <adelaney27@yahoo.com>
2020-01-18 07:44:53 -0600

    Update settings.py and add Procfile

--- taskmate/settings.py
+++ taskmate/settings.py
@@ -125,6 +125,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # resolves initial heroku push error
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
@@ -134,3 +135,4 @@ LOGIN_REDIRECT_URL = 'todolist'
 LOGIN_URL = 'login'
 
 django_heroku.settings(locals())
+
